a514b93a57b1e2cd63fe640061e5d385281c410c
Lucas Wagner <lgwagner@gmail.com>
2020-05-19 16:17:36 -0500

    minor fixes to address insufficiencies in Kind2 analysis.
    - local installs now work.

--- plugin/com.collins.trustedsystems.jkindapi/src/jkind/api/xml/XmlParseThread.java
+++ plugin/com.collins.trustedsystems.jkindapi/src/jkind/api/xml/XmlParseThread.java
@@ -316,8 +316,9 @@ public class XmlParseThread extends Thread {
 	protected int getTime(int parsed) {
 		switch (backend) {
 		case JKIND:
-			return parsed;
 		case KIND2:
+			return parsed;
+
 		case SALLY:
 			return parsed - 1;
 		default:
