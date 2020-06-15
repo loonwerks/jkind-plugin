fcf36a2fc8a7c00c7b857f7efa95e0cca9b325dc
Karl Hoech <karl.hoech@collins.com>
2020-06-15 15:30:31 -0500

    Eliminate release fetcher as this repo is now canonical location for JKind API

--- git-setup.sh
+++ git-setup.sh
@@ -3,5 +3,5 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git remote add origin-with-token https://${GH_TOKEN}@github.com/loonwerks/formal-methods-workbench.git > /dev/null 2>&1
+git remote add origin-with-token https://${GH_TOKEN}@github.com/loonwerks/jkind-plugin.git > /dev/null 2>&1
 
